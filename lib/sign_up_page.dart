88e723389d2ab60776cd241620654c8ffc1a1f2b
seungju lee <dltmdwn0322@gmail.com>
2024-07-06 01:02:14 +0900

    회원가입 페이지 디자인 수정

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -16,6 +16,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final _bioController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
+  bool _passwordVisible = false;
+  bool _rePasswordVisible = false;
 
   Future<void> _signup() async {
     if (_formKey.currentState!.validate()) {
@@ -55,83 +57,202 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
   }
 
+  InputDecoration _inputDecoration(String labelText) {
+    return InputDecoration(
+      labelText: labelText,
+      floatingLabelBehavior: FloatingLabelBehavior.auto,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: Colors.grey,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: Color(0xFF495ECA),
+        ),
+      ),
+      labelStyle: TextStyle(fontSize: 14),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Signup'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'ID'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your user ID';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _rePasswordController,
-                decoration: InputDecoration(labelText: 'Re-enter Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please re-enter your password';
-                  }
-                  if (value != _passwordController.text) {
-                    return 'Passwords do not match';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: 'Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _bioController,
-                decoration: InputDecoration(labelText: 'Bio (optional)'),
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email (optional)'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                controller: _phoneController,
-                decoration: InputDecoration(labelText: 'Phone (optional)'),
-                keyboardType: TextInputType.phone,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _signup,
-                child: Text('Sign Up'),
-              ),
-            ],
+      backgroundColor: Colors.white,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                Text('Sign up',
+                    style: TextStyle(
+                        fontSize: 42,
+                        color: Color(0xFF495ECA),
+                        fontWeight: FontWeight.bold)),
+                SizedBox(height: 8),
+                Text('Please create a new account',
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: _inputDecoration('ID'),
+                  style: TextStyle(fontSize: 14),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your user ID';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Color(0xFF495ECA),
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                    labelStyle: TextStyle(fontSize: 14),
+                  ),
+                  obscureText: !_passwordVisible,
+                  style: TextStyle(fontSize: 14),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _rePasswordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password 확인',
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Color(0xFF495ECA),
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _rePasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _rePasswordVisible = !_rePasswordVisible;
+                        });
+                      },
+                    ),
+                    labelStyle: TextStyle(fontSize: 14),
+                  ),
+                  obscureText: !_rePasswordVisible,
+                  style: TextStyle(fontSize: 14),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please re-enter your password';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _nameController,
+                  decoration: _inputDecoration('이름'),
+                  style: TextStyle(fontSize: 14),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _bioController,
+                  decoration: _inputDecoration('Bio (Optional)'),
+                  style: TextStyle(fontSize: 14),
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: _inputDecoration('Email (Optional)'),
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 14),
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: _phoneController,
+                  decoration: _inputDecoration('Phone (Optional)'),
+                  keyboardType: TextInputType.phone,
+                  style: TextStyle(fontSize: 14),
+                ),
+                SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: _signup,
+                  child: Text('Sign up',
+                      style: TextStyle(color: Colors.white, fontSize: 18)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF495ECA),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
