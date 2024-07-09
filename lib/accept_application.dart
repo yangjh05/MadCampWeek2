fe045ea932b333d418ff10e501acd8c5aca566cd
Jinhyeok Yang <yangjh05@kaist.ac.kr>
2024-07-10 00:30:50 +0900

    Accept/Decline New Teammate

--- lib/accept_application.dart
+++ lib/accept_application.dart
@@ -60,6 +60,153 @@ class _AcceptApplicationState extends State<AcceptApplication> {
     }
   }
 
+  Future<void> applyResult(int user_id, int isAccept) async {
+    dynamic res = [];
+    print(user_id);
+    final response = await http.post(
+      Uri.parse("https://172.10.7.95/api/apply_result"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'user_id': user_id.toString(),
+        'org_id': org_info['organization_id'].toString(),
+        'appRes': isAccept.toString()
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        print("Success");
+        res = jsonDecode(response.body);
+        print(res);
+      });
+
+      if (isAccept == 0) {
+        return;
+      }
+
+      // 각 역할의 사용자 목록에서 사용자의 이름만 추출
+      List<Map<String, dynamic>> getSecondDropdownItems(dynamic userList) {
+        print(userList);
+        return userList.map<Map<String, dynamic>>((user) {
+          print(user);
+          return {
+            'user_role_id': user['user_role_id'],
+            'username': user['username']
+          };
+        }).toList();
+      }
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          int dropdownValue1 = res['roles'][0]['role_id'];
+          int? dropdownValue2;
+
+          print(dropdownValue1);
+
+          // 첫 번째 역할의 사용자 목록에서 사용자 이름만 추출
+
+          List<Map<String, dynamic>> secondDropdownItems =
+              getSecondDropdownItems(res['userList'][0]['users']);
+
+          return AlertDialog(
+            title: Text('직급을 선택하세요'),
+            content: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    DropdownButton<int>(
+                      value: dropdownValue1,
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          print("new Value");
+                          print(newValue);
+
+                          dropdownValue1 = newValue!;
+                          dropdownValue2 = null;
+
+                          print("Yay");
+
+                          // 새로운 역할에 따라 두 번째 드롭다운 항목 업데이트
+                          secondDropdownItems = getSecondDropdownItems(
+                            res['userList'].firstWhere((element) {
+                              print(element);
+                              return element['role_id'] == newValue;
+                            })['users'],
+                          );
+                        });
+                      },
+                      items: res['roles'].map<DropdownMenuItem<int>>((value) {
+                        return DropdownMenuItem<int>(
+                          value: value['role_id'],
+                          child: Text(value['role_name']),
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButton<int>(
+                      value: dropdownValue2,
+                      hint: Text('직속 상사를 선택하세요'),
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          dropdownValue2 = newValue!;
+                          print(dropdownValue2);
+                        });
+                        print('Selected user_role_id: $dropdownValue2');
+                      },
+                      items: secondDropdownItems
+                          .map<DropdownMenuItem<int>>((value) {
+                        return DropdownMenuItem<int>(
+                          value: value['user_role_id'],
+                          child: Text(value['username']),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                );
+              },
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('확인'),
+                onPressed: () async {
+                  print("Ok pressed");
+                  print(dropdownValue2);
+                  if (dropdownValue2 == null) return;
+                  Navigator.of(context).pop();
+                  final finalres = await http.post(
+                    Uri.parse("https://172.10.7.95/api/apply_user_org"),
+                    headers: <String, String>{
+                      'Content-Type': 'application/json; charset=UTF-8',
+                    },
+                    body: jsonEncode(<String, String>{
+                      'org_role_id': dropdownValue1.toString(),
+                      'user_id': user_id.toString(),
+                      'par_user_role': dropdownValue2
+                          .toString(), // 두 번째 드롭다운에서 선택된 user_role_id 값을 전달
+                    }),
+                  );
+                  print(finalres.body);
+                  setState(() {
+                    info_list.removeWhere((item) => item['user_id'] == user_id);
+                  });
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to get response: ${response.body}')),
+      );
+      throw Exception();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,7 +267,22 @@ class _AcceptApplicationState extends State<AcceptApplication> {
                                         children: [
                                           ElevatedButton(
                                             onPressed: () {
-                                              // 버튼을 눌렀을 때 실행될 코드 작성
+                                              print(
+                                                  "Member user id : ${member['user_id']}");
+                                              applyResult(member['user_id'], 2);
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                                              minimumSize: Size(30,
+                                                  30), // 너비와 높이를 설정하여 버튼 크기 조정
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal:
+                                                      8), // 버튼 내용의 패딩 설정
+                                            ),
+                                            child: Icon(Icons.account_circle),
+                                          ),
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              applyResult(member['user_id'], 1);
                                             },
                                             style: ElevatedButton.styleFrom(
                                               minimumSize: Size(30,
@@ -133,7 +295,7 @@ class _AcceptApplicationState extends State<AcceptApplication> {
                                           ),
                                           ElevatedButton(
                                             onPressed: () {
-                                              // 버튼을 눌렀을 때 실행될 코드 작성
+                                              applyResult(member['user_id'], 0);
                                             },
                                             style: ElevatedButton.styleFrom(
                                               minimumSize: Size(30,
